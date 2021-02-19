20a236d944099c3de0b9d28563a0ad6f41593bf6
Ollie Robins <ollierobins93@gmail.com>
2021-02-19 11:00:25 +0000

    happy with typescript tests added

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
 import React, {FC} from 'react';
+import TypeScriptTest from './components/TyprescriptTest';
+
 
 const App : FC = () => {
     return (
         <div>
             <h1>This is now working</h1>
+            <TypeScriptTest/>
         </div>
     )
 }
